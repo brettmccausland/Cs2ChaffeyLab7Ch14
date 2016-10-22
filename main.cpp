5a613405d9e82cd2f2dfc9eeab931cae185c2a62
brettmccausland <brettmccausland@yahoo.com>
2016-10-21 20:02:21 -0700

    Included Exercises and Directions

---
+++ main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+void Exercises14_1();
+void Exercises14_2();
+void Exercises14_3();
+void Exercises14_6();
+void Exercises14_8();
+
+using namespace std;
+
+int main()
+{
+  system("clear");
+
+
+
+  return 0;
+}
+void Exercises14_1()
+{
+  // Finish the implementation of the class Fraction
+  // by overloading the remaining arithmetic operations.
+
+}
+void Exercises14_2()
+{
+  // Modify the class Fraction to permit fractions with a zero
+  // denominator. Change the stream output operator so that it will
+  // produce a special marker, *****,when such a value is printed.
+  // Modify the conversion operator double so that it will produce the value
+  // zero in this case.Finally,add a conversion operator bool so as to test
+  // whether a Fraction is proper.
+}
+void Exercises14_3()
+{
+  // Determine which functions in class Fraction
+  // are candidates for inlining. Rewrite the class to inline
+  // those you identify, and test the resulting application.
+}
+void Exercises14_6()
+{
+    //  Define a class Complex for complex numbers. Provide
+    //  implementations for addition, subtraction, multiplication,
+    //  and the stream input and output operators.
+    //  Implement the compound assignment operators for each of the supported
+    //  binary arithmetic operations.
+}
+void Exercises14_8()
+{
+  // Implement a class for polynomials. Store the coefficients in a
+  // vector of floating-point values. Then provide operators for addition,
+  // subtraction, multipli- cation, and output.
+}
